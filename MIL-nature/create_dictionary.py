00992cabc80f12e8b5c56ab74a219d3c9699f708
olga.fourkioti <olgafour1@gmail.com>
2021-12-23 15:04:08 +0000

    path correction

--- MIL-nature/create_dictionary.py
+++ MIL-nature/create_dictionary.py
@@ -62,7 +62,7 @@ def load_bags(wsi_path, train,csv_file):
                 h5_coords = hdf5_file['coords'][:]
 
 
-        return h5_coords.tolist(), int(bag_label)
+        return map(tuple, h5_coords), int(bag_label)
 
 def create_dict(filenames, train, dict_name,csv_file=None):
 
